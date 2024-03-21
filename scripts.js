d25a2ae51105f9974bc12f98a6355f77dcc10f8d
hinzdc <devilcyber33@gmail.com>
2024-03-22 01:55:17 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function decryptECB() {
     const key = document.getElementById("key").value;
     const keyLength = parseInt(document.getElementById("keyLength").value);
     const options = { mode: CryptoJS.mode.ECB, keySize: keyLength / 8 };
-    const bytes = CryptoJS.AES.decrypt(ciphertext, key, options);
-    const decryptedText = bytes.toString(CryptoJS.enc.Utf8);
+    const decryptedBytes = CryptoJS.AES.decrypt(ciphertext, key, options);
+    const decryptedText = decryptedBytes.toString(CryptoJS.enc.Utf8);
     document.getElementById("outputText").value = decryptedText;
 }
